ddc6e86f0fb06adb8c6480462de2a6a95813fdd8
mianamir <mianamirlahore@gmail.com>
2020-11-07 12:42:18 -0700

    React conditionals

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,26 +1,43 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count: 0,
+    count: 1,
     image_url: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"],
   };
 
   styles = {
-    fontSize: 10,
+    fontSize: 12,
     fontWeight: "bold",
   };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
 
   render() {
     return (
       <React.Fragment>
-        {/* <img src={this.state.image_url} alt="" /> */}
-        <span style={this.styles} className="badge badge-primary m-2">
-          {this.format_count()}
-        </span>
+        <span className={this.get_badge_classes()}>{this.format_count()}</span>
         <button className="btn btn-secondary btn-sm">Increement</button>
+        {this.renderTags()};
       </React.Fragment>
     );
   } // render ends
+
+  get_badge_classes() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 
   format_count() {
     const { count } = this.state;
