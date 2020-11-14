4c98cc6c5498b851be535222c93558d7f3cc327d
mianamir <mianamirlahore@gmail.com>
2020-11-14 10:59:59 -0700

    embeding expressions, handling conditionals using JSX expressions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ class Counter extends Component {
   state = {
     count: 1,
     image_url: "https://picsum.photos/200",
-    tags: ["tag1", "tag2", "tag3"],
+    tags: [],
   };
 
   styles = {
@@ -16,6 +16,7 @@ class Counter extends Component {
 
     return (
       <ul>
+        {/* To iterate lists use map and arrow fun */}
         {this.state.tags.map((tag) => (
           <li key={tag}>{tag}</li>
         ))}
@@ -26,20 +27,27 @@ class Counter extends Component {
   render() {
     return (
       <React.Fragment>
-        <span className={this.get_badge_classes()}>{this.format_count()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increement</button>
+        <br />
+        {/* In JS when we use boolean logical operator 
+        && / || / ! operators then concatinating
+        boolean with string will rsult in string 
+        becuase this will be truthy statement
+        */}
+        {this.state.tags.length === 0 && "Would you please a new tag!"}
         {this.renderTags()};
       </React.Fragment>
     );
   } // render ends
 
-  get_badge_classes() {
+  getBadgeClasses() {
     let classes = "badge m-2 badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
   }
 
-  format_count() {
+  formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
   }
